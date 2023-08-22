2b1c904dac0dfb9fe11fc07cab10c4b18af72399
michaelakuempel <43602731+michaelakuempel@users.noreply.github.com>
2023-08-22 12:03:58 +0200

    Update __init__.pl

--- src/__init__.pl
+++ src/__init__.pl
@@ -1,12 +1,12 @@
 % You can register other knowrob packages here. 
 :- register_ros_package(knowrob).
-:- register_ros_package(knowrob_example).
+:- register_ros_package(knowrob_KG_cutting).
 
 % Load our owl file and define a namespace that can be used later, e.g.
 % in model/objects.pl we write knowrobexample:Pizza instead of the full
 % URI ('http://www.ease-crc.org/ont/knowrob-example#Pizza')
-:- load_owl('package://knowrob_example/owl/knowrob-example.owl',
-	[namespace(krexample,'http://www.ease-crc.org/ont/knowrob-example#')]).
+%:- load_owl('package://knowrob_example/owl/knowrob-example.owl',
+%	[namespace(krexample,'http://www.ease-crc.org/ont/knowrob-example#')]).
 
 % Load your subdirectories here
 :- use_directory('util').
