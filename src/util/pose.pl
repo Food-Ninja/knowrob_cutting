5aff68d5e95cd7650ec47a1775c85427fe9db560
michaelakuempel <43602731+michaelakuempel@users.noreply.github.com>
2023-08-25 18:13:20 +0200

    Update pose.pl

--- src/util/pose.pl
+++ src/util/pose.pl
@@ -2,7 +2,7 @@
 % argument
 :- module(pose,
     [ 
-      position_to_be_used/1
+      position_to_be_used/2
     ]).
 
 :- use_module(library(semweb/rdf_db)), 
@@ -30,5 +30,5 @@
 % @param Action The action to be performed (cutting, halving, quartering, slicing, julienning, dicing)
 % @param Pose The pose to be used
 %
-position_to_be_used(Action,Pose)?> fc ??
-  rdfs_subclass_of(?node, Action), rdf(?node, owl:'onProperty', cut:'requiresPosition'), rdf(?node, owl:'someValuesFrom', Pose).
+position_to_be_used(Action,Pose):- fc ??
+  rdfs_subclass_of(Node, Action), rdf(Node, owl:'onProperty', cut:'requiresPosition'), rdf(Node, owl:'someValuesFrom', Pose).
